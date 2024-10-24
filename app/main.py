67f82eaa9ac852fcb9af54a32957d1241289901f
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2024-10-24 17:11:10 +0900

    Refactor draggacle_data keys and values

--- app/main.py
+++ app/main.py
@@ -12,13 +12,13 @@ def main(page: ft.Page):
     page.scroll = "always"
 
     draggacle_data = {
-        "_197": "処方修正",
-        "_201": "医師からの問い合わせ",
-        "_205": "看護師からの問い合わせ",
-        "_209": "薬剤セット数",
-        "_213": "持参薬を確認",
-        "_217": "薬剤服用歴等について保険薬局へ照会",
-        "_213": "TDM実施",
+        "_245": "処方修正",
+        "_249": "医師からの問い合わせ",
+        "_253": "看護師からの問い合わせ",
+        "_257": "薬剤セット数",
+        "_261": "持参薬を確認",
+        "_265": "薬剤服用歴等について保険薬局へ照会",
+        "_269": "TDM実施",
     }
 
     range_values = {}
@@ -29,9 +29,10 @@ def main(page: ft.Page):
         print(src_id)
         key = draggacle_data.get(src_id, "")
         e.control.content = ft.Container(
-            ft.Text(key),
+            ft.Text(key, color="white"),
             width=50,
-            height=50,
+            height=90,
+            bgcolor=ft.colors.BLUE_GREY_500,
         )
         e.control.update()
         range_values.setdefault(e.control.data, key)
@@ -45,18 +46,84 @@ def main(page: ft.Page):
                 writer.writerow([key, value])
 
     save_button = ft.ElevatedButton(text="Save", on_click=write_csv_file)
-    graph_Space = ft.Container()
-    selected_files = ft.Text()
 
     def pick_file_result(e: ft.FilePickerResultEvent):
-        print(e.files)
+        if e.files:
+            selected_files.text = ",".join(map(lambda x: x.name, e.files))
+            file_paths = [f.path for f in e.files]
+            try:
+                # 空のデータフレームを作成
+                df = pd.DataFrame()
+                # ファイルの数だけ繰り返す
+                df = pd.concat([pd.read_csv(file_path) for file_path in file_paths])
+                # Task ごとにまとめる
+                groupby_task = df.groupby("Task").size().reset_index(name="Count")
+                print(groupby_task)
+                # グラフを描画
+                bar_charts = [
+                    ft.BarChartGroup(
+                        x=i,
+                        bar_rods=[
+                            ft.BarChartRod(
+                                from_y=0,
+                                to_y=row["Count"],
+                                color="blue",
+                                border_radius=0,
+                            )
+                        ],
+                    )
+                    for i, row in groupby_task.iterrows()
+                ]
+                x_labels = [
+                    ft.ChartAxisLabel(
+                        value=i,
+                        label=ft.Container(
+                            ft.Text(row["Task"]), padding=ft.Padding(0, 0, 0, 0)
+                        ),
+                    )
+                    for i, row in groupby_task.iterrows()
+                ]
+                bar_chart.bar_groups = bar_charts
+                bar_chart.bottom_axis.labels = x_labels
+                bar_chart.update()
+            except Exception as e:
+                print(e)
 
     file_picker = ft.FilePicker(on_result=pick_file_result)
+    page.overlay.append(file_picker)
 
+    selected_files = ft.Text()
     file_picker_Button = ft.ElevatedButton(
         "ファイルを選択",
         on_click=lambda _: file_picker.pick_files(allow_multiple=True),
     )
+
+    bar_chart = ft.BarChart(
+        bar_groups=[],
+        border=ft.border.all(1, ft.colors.GREEN_100),
+        left_axis=ft.ChartAxis(labels_size=40, title=ft.Text("Count"), title_size=20),
+        bottom_axis=ft.ChartAxis(labels_size=40),
+        horizontal_grid_lines=ft.ChartGridLines(
+            color=ft.colors.GREEN_100, width=1, dash_pattern=[3, 3]
+        ),
+        tooltip_bgcolor=ft.colors.with_opacity(0.5, ft.colors.GREEN_100),
+        max_y=10,
+        interactive=True,
+        expand=True,
+    )
+
+    hours = [
+        "8:00",
+        "9:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+    ]
 
     times = [
         "8:30 8:40",
@@ -106,25 +173,31 @@ def main(page: ft.Page):
         "16:20 16:30",
         "16:30 16:40",
         "16:40 16:50",
-        "16:50 17:00",]
+        "16:50 17:00",
+    ]
     columns = []
     for time in times:
         columns.append(
-            ft.Column(
-                [
-                    ft.Text(time, size=10),
-                    ft.DragTarget(
-                        group="timeline",
-                        content=ft.Container(
-                            width=50,
-                            height=50,
-                            bgcolor=ft.colors.BLUE_GREY_100,
-                            border_radius=5,
+            ft.Container(
+                ft.Column(
+                    [
+                        ft.Text(time, size=12),
+                        ft.DragTarget(
+                            group="timeline",
+                            content=ft.Container(
+                                width=50,
+                                height=90,
+                                bgcolor=None,
+                                border_radius=5,
+                            ),
+                            data=time,
+                            on_accept=drag_accepted,
                         ),
-                        data=time,
-                        on_accept=drag_accepted,
-                    ),
-                ]
+                    ],
+                    spacing=0,
+                ),
+                margin=0,
+                padding=0,
             )
         )
 
@@ -145,9 +218,9 @@ def main(page: ft.Page):
                     ft.Draggable(
                         group="timeline",
                         content=ft.Container(
-                            ft.Text(kind),
-                            width=50,
-                            height=50,
+                            ft.Text(kind, color="white"),
+                            width=100,
+                            height=70,
                             bgcolor=ft.colors.BLUE_GREY_500,
                             border_radius=5,
                         ),
@@ -166,7 +239,7 @@ def main(page: ft.Page):
         save_button,
         file_picker_Button,
         selected_files,
-        graph_Space,
+        bar_chart,
     )
 
 
