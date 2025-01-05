64f36c19a3b39cd337366f28a00238219371d5e8
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2025-01-05 20:39:28 +0900

    病棟データは複数選択方式に変更

--- app/components/components.py
+++ app/components/components.py
@@ -2,13 +2,15 @@ import flet as ft
 import datetime
 from models.models import DataModel
 from handlers.handlers import Handlers
+
+
 class DateComponent:
-    def __init__(self,page,today,handle_change):
+    def __init__(self, page, today, handle_change):
         self.page = page
         self.today = today
         self.handle_change = handle_change
-        
-    def create(self): 
+
+    def create(self):
         return ft.ElevatedButton(
             text=f"{self.today.year}/{self.today.month}/{self.today.day}",
             icon=ft.icons.CALENDAR_MONTH,
@@ -17,136 +19,136 @@ class DateComponent:
                     first_date=datetime.date(
                         year=self.today.year, month=self.today.month, day=self.today.day
                     ),
-                    on_change=self.handle_change
+                    on_change=self.handle_change,
                 )
-            )
+            ),
         )
 
+
 class NameDropdown:
-    def __init__(self,page,phNameList,dropdown_changed):
+    def __init__(self, page, phNameList, dropdown_changed):
         self.page = page
         self.phNameList = phNameList
-        self.on_change = dropdown_changed        
+        self.on_change = dropdown_changed
+
     def create(self):
         return ft.Dropdown(
             width=130,
             options=[],
-            on_change = self.on_change,
+            on_change=self.on_change,
             label="Name",
             text_size=12,
             label_style=ft.TextStyle(size=12),
             border_color=ft.colors.BLUE_GREY_100,
-            height=40
+            height=40,
         )
 
+
 class EndDrawer:
-    def __init__(self,page):
+    def __init__(self, page):
         self.page = page
-    def  create(self):
+
+    def create(self):
         return ft.NavigationDrawer(
             position=ft.NavigationDrawerPosition.END,
-            controls = [],
+            controls=[],
         )
+
 
 class AmDropDown:
     def create(self):
-        return ft.Dropdown(
-            width=130,
-            options=[
-                ft.dropdown.Option("ICU"),
-                ft.dropdown.Option("3A"),
-                ft.dropdown.Option("3B"),
-                ft.dropdown.Option("3C"),
-                ft.dropdown.Option("CCU"),
-                ft.dropdown.Option("4A"),
-                ft.dropdown.Option("4B"),
-                ft.dropdown.Option("4C"),
-                ft.dropdown.Option("HCU"),
-                ft.dropdown.Option("5A"),
-                ft.dropdown.Option("5B"),
-                ft.dropdown.Option("5C"),
-                ft.dropdown.Option("5D"),
+        return ft.Column(
+            [
+                ft.Checkbox(label="ICU"),
+                ft.Checkbox(label="3A"),
+                ft.Checkbox(label="3B"),
+                ft.Checkbox(label="3C"),
+                ft.Checkbox(label="CCU"),
+                ft.Checkbox(label="4A"),
+                ft.Checkbox(label="4B"),
+                ft.Checkbox(label="4C"),
+                ft.Checkbox(label="4D"),
+                ft.Checkbox(label="HCU"),
+                ft.Checkbox(label="5A"),
+                ft.Checkbox(label="5B"),
+                ft.Checkbox(label="5C"),
+                ft.Checkbox(label="5D"),
+            ]
+        )
 
-                ],
-            label="AM",
-            text_size=12,
-            label_style=ft.TextStyle(size=12),
-            border_color=ft.colors.BLUE_GREY_100,
-            height=40
-    )
-    
+
 class PmDropDown:
     def create(self):
-        return ft.Dropdown(
-        width=130,
-        options=[
-            ft.dropdown.Option("ICU"),
-            ft.dropdown.Option("3A"),
-            ft.dropdown.Option("3B"),
-            ft.dropdown.Option("3C"),
-            ft.dropdown.Option("CCU"),
-            ft.dropdown.Option("4A"),
-            ft.dropdown.Option("4B"),
-            ft.dropdown.Option("4C"),
-            ft.dropdown.Option("HCU"),
-            ft.dropdown.Option("5A"),
-            ft.dropdown.Option("5B"),
-            ft.dropdown.Option("5C"),
-            ft.dropdown.Option("5D"),
+        return ft.Column(
+            [
+                ft.Checkbox(label="ICU"),
+                ft.Checkbox(label="3A"),
+                ft.Checkbox(label="3B"),
+                ft.Checkbox(label="3C"),
+                ft.Checkbox(label="CCU"),
+                ft.Checkbox(label="4A"),
+                ft.Checkbox(label="4B"),
+                ft.Checkbox(label="4C"),
+                ft.Checkbox(label="HCU"),
+                ft.Checkbox(label="5A"),
+                ft.Checkbox(label="5B"),
+                ft.Checkbox(label="5C"),
+                ft.Checkbox(label="5D"),
+            ]
+        )
 
-            ],
-        label="PM",
-        text_size=12,
-        label_style=ft.TextStyle(size=12),
-        border_color=ft.colors.BLUE_GREY_100,
-        height=40
-    )
-        
-#editButton
+
+# editButton
 class EditButton:
-    def __init__(self,page,toggle_delete_button):
+    def __init__(self, page, toggle_delete_button):
         self.page = page
         self.toggle_delete_button = toggle_delete_button
-        
-    def create(self,e):
+
+    def create(self, e):
         return ft.IconButton(
-            icon=ft.icons.EDIT,
-            on_click=lambda e:self.toggle_delete_button
+            icon=ft.icons.EDIT, on_click=lambda e: self.toggle_delete_button
         )
-        
+
+
 class DeleteButtons:
-    def __init__(self,page,delete_content):
+    def __init__(self, page, delete_content):
         self.page = page
         self.delete_content = delete_content
+
     def create(self):
         return ft.IconButton(
             icon=ft.icons.DELETE,
             visible=False,
             icon_color="red",
             icon_size=20,
-            on_click = lambda e:self.delete_content(e),
+            on_click=lambda e: self.delete_content(e),
         )
-        
+
+
 class CreateCounter:
-    def __init__(self,page,count_field):
+    def __init__(self, page, count_field):
         self.page = page
         self.count_field = count_field
-        
-    def create(self,e):
+
+    def create(self, e):
         return ft.Column(
             [
                 ft.IconButton(
                     icon=ft.icons.ARROW_DROP_UP_OUTLINED,
                     icon_size=25,
-                    on_click=lambda _: self.counterPlus(e,self.count_field,count_dict),
-                    style = ft.ButtonStyle(shape = ft.RoundedRectangleBorder(radius = 5)),
+                    on_click=lambda _: self.counterPlus(
+                        e, self.count_field, count_dict
+                    ),
+                    style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=5)),
                 ),
                 self.count_field,
                 ft.IconButton(
                     ft.icons.ARROW_DROP_DOWN_OUTLINED,
-                    icon_size = 25,
-                    on_click = lambda _: self.counterMinus(e,self.count_field,count_dict),
-                    style = ft.ButtonStyle(shape=ft.RoundedReactangleBorder(radius=5)),
+                    icon_size=25,
+                    on_click=lambda _: self.counterMinus(
+                        e, self.count_field, cont_dict
+                    ),
+                    style=ft.ButtonStyle(shape=ft.RoundedReactangleBorder(radius=5)),
                 ),
             ]
         )
