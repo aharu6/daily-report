862bb47103abe45e215aadf5199dfd117f020e93
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2025-01-08 08:28:32 +0900

    カラム削除時に一番左のカラムのテキスト表示が残るように修正

--- app/handlers/reload_data.py
+++ app/handlers/reload_data.py
@@ -1,11 +1,3 @@
-import json
-import datetime
-import flet as ft
-import csv
-import pandas as pd
-from models.models import DataModel
-
-
 #ドロワーを展開する
 #保管しているデータを取得して表示する
 #右側にtimeline適用用のボタンを合わせて表示する
