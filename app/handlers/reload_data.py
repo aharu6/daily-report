579f90c3f483842152b85296f99723cb0805b22f
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2025-01-15 09:13:41 +0900

    client_storageの保存形式を変更 保存データに日付と名前のキーを入れて一意に変更

--- app/handlers/reload_data.py
+++ app/handlers/reload_data.py
@@ -5,5 +5,9 @@ class ReloadDataHandler:
     @staticmethod
     def toggle_Reload_Data(e,page,drawer):
         page.open(drawer)
+        #保存しているデータを読み出す
+        #write csv時の保存名：timeline_data
+        dat = page.client_storage.get("timeline_data")
+        print(dat)
         
     
